d1be4e22faf84f20bd8d0fedc94e6944d0098274
William <wlino@hcor.com.br>
2019-12-20 13:53:08 -0300

    acertar o entrypoint.sh

--- laravel/.docker/entrypoint.sh
+++ laravel/.docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-RUN composer install
-RUN cp .env.example .env
-RUN php artisan key:generate
-RUN php artisan config:cache
-RUN php artisan migrate 
+composer install
+cp .env.example .env
+php artisan key:generate
+php artisan config:cache
+php artisan migrate 
 php artisan migrate
 php-fpm
